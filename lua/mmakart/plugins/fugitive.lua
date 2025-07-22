c5feae426de56b2cb3e0be181e794846223b65b6
Maksym Makartsov <try30011997@gmail.com>
2025-07-22 22:51:30 +0300

    push kebind

--- lua/mmakart/plugins/fugitive.lua
+++ lua/mmakart/plugins/fugitive.lua
@@ -3,6 +3,7 @@ return {
     'tpope/vim-fugitive',
     config = function()
       vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>')
+      vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>')
     end,
   },
 }

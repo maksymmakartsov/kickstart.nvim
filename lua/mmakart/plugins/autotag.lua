a26206dc9c08d995d823b051019fd2cd4173f33d
Maksym Makartsov <try30011997@gmail.com>
2025-09-05 13:48:52 +0300

    nvchad config

--- lua/mmakart/plugins/autotag.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'windwp/nvim-ts-autotag',
-  -- No specific configuration is needed for basic functionality
-  opts = {},
-}
--- lua/mmakart/plugins/undotree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    'mbbill/undotree',
-    config = function()
-      vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<CR>', { desc = 'Toggle [U]ndo tree window' })
-    end,
-  },
-}

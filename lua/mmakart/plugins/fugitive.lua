b64aefa8a8bbc9d3853e712b5d7ef43944b70d2d
Maksym Makartsov <try30011997@gmail.com>
2025-09-05 13:41:44 +0300

    fugitive keybind

--- lua/mmakart/plugins/fugitive.lua
+++ lua/mmakart/plugins/fugitive.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>')
       vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>')
+      vim.keymap.set('n', '<leader>gg', '<cmd>Git pull<CR>')
     end,
   },
 }
